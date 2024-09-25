76a23039db2653e0164ec4dd4f9b1cbe2fdbbebf
ChrisPaoloSab-it <cpaocastillos@gmail.com>
2024-09-25 15:32:03 +0800

    added functionality, will try to change layout to adpat to landscape

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.artspaceapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,25 @@ class SplashScreen : AppCompatActivity() {
     }
 }
 
+
     class MainActivity : AppCompatActivity() {
+
+        private lateinit var artPiece: ImageView
+        private lateinit var artTitle: TextView
+        private lateinit var artist: TextView
+
+        private data class Description(val imageResId: Int, val title: String, val artist: String)
+
+        private val Gallery = listOf(
+            Description(R.drawable.baler, "Baler on a Cloudy Day", "Pao (2017)"),
+            Description(R.drawable.jobi, "Moonlit Mascot", "Pao (2019)"),
+            Description(R.drawable.oreo, "Oreo", "Pao (2024)"),
+            Description(R.drawable.ramshorn, "Slow Life", "Pao 2019"),
+            Description(R.drawable.ulap, "Ulap", "Pao (2017)")
+        )
+
+        private var currentIndex = 0
+
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             enableEdgeToEdge()
@@ -32,5 +54,30 @@ class SplashScreen : AppCompatActivity() {
                 v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
                 insets
             }
+
+            artPiece = findViewById(R.id.ArtPiece)
+            artTitle = findViewById(R.id.ArtTitle)
+            artist = findViewById(R.id.Artist)
+
+            val previousButton = findViewById<Button>(R.id.PreviousButton)
+            previousButton.setOnClickListener {
+                currentIndex = (currentIndex - 1 + Gallery.size) % Gallery.size
+                displayArt()
+            }
+            val nextButton = findViewById<Button>(R.id.NextButton)
+            nextButton.setOnClickListener {
+                currentIndex = (currentIndex + 1) % Gallery.size
+                displayArt()
+            }
+            displayArt()
+
+        }
+
+        private fun displayArt() {
+            val currentDescription = Gallery[currentIndex]
+            artPiece.setImageResource(currentDescription.imageResId)
+            artTitle.text = currentDescription.title
+            artist.text = currentDescription.artist
         }
     }
+
